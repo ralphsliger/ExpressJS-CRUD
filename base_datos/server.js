0431031bed7e68d8a5eab3f1104a5df2ae3e880c
Ralph Sliger <ralphsliger1@gmail.com>
2019-01-10 23:28:22 -0500

    "correcion"
    correcion

--- base_datos/server.js
+++ base_datos/server.js
@@ -5,11 +5,24 @@ const Sequelize = require('sequelize');
 
 const app = express();
 
+//const tasks = require('./controllers/tasks'); //importar controlador tasks
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+//const sequelize = new Sequelize('proyecto-backend',null,null,{ // iniciar conexion  a bd
+  //dialect: 'sqlite',
+  // storage: './proyecto-backend'
+// });
+//let db = new sqlite3.Database('proyecto-backend');
+
 app.set('view engine','pug'); //vistas
+
+//app.get('/tasks',tasks.home); 
+
+app.post('/pendientes', function(req,res){
+  //db.run(`INSERT INTO tasks(description) VALUES(?)`,req.body.description);
+  res.send('Insercion final')
+});
 
 
 app.listen(3000);
-
