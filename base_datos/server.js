cadacabbeb6c17756b74ba5224cc57d4f1b5f2b4
Ralph Sliger <ralphsliger1@gmail.com>
2019-01-10 23:45:58 -0500

    get en subruta tasks

--- base_datos/server.js
+++ base_datos/server.js
@@ -5,11 +5,13 @@ const Sequelize = require('sequelize');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+const tasksRoutes = require('./routes/task_routes'); //importar grupo rutas 'task routes subrutas'
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine','pug'); //vistas
 
+app.use(tasksRoutes);
 
 
 app.listen(3000);
